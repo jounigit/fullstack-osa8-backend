61ca05aeab198d6685c5676ef3e5c16f9232b23c
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2019-03-04 09:06:12 +0200

    Osa 8.3

--- index.js
+++ index.js
@@ -91,11 +91,19 @@ const typeDefs = gql`
 	  genres: [String]
       id: ID!
   }
+
+  type Author {
+	  name: String!
+	  born: Int
+	  id: ID!
+	  bookCount: Int		
+  }
   
   type Query {
 	  bookCount: Int!
 	  authorCount: Int!
 	  allBooks: [Book!]!
+	  allAuthors: [Author!]!
   }
 `
 
@@ -103,7 +111,14 @@ const resolvers = {
   Query: {
 	  bookCount: () => books.length,
 	  authorCount: () => authors.length,
-	  allBooks: () => books
+	  allBooks: () => books,
+	  allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+	  console.log(books.filter(b => b.author === root.name))
+      return books.filter(b => b.author === root.name).length
+    }
   }
 }
 
